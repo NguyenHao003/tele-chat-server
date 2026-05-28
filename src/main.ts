e1c56a9f1c52633daba03071276f7a7a711fa133
NguyenHao <haond.dev@ant-group.net>
2026-05-28 08:20:30 +0700

    chore: test

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
   })
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
   await app.listen(process.env.PORT ?? 3000)
+  console.log(`Application is running on: ${process.env.PORT}`)
 }
 
 bootstrap()
